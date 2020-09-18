4558f38c865a5294d48754cf94414a6ddab0d225
wildenali <wildenwildenaliali@gmail.com>
2020-09-18 15:31:51 +0700

    Create a SidebarChat.css

---
+++ src/SidebarChat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SidebarChat.css";
+
+function SidebarChat() {
+  return (
+    <div className="sidebarChat">
+      <h1>Add new Chat</h1>
+    </div>
+  );
+}
+
+export default SidebarChat;
