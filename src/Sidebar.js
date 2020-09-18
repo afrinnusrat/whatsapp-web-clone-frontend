9f243e4476052d194869ec9380e7a2899a674581
wildenali <wildenwildenaliali@gmail.com>
2020-09-18 14:25:48 +0700

    Make the DonutIcon, ChatIcon and MoreVertIcon to be Clickable button

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,24 @@
 import React from "react";
+import "./Sidebar.css";
 import ChatIcon from "@material-ui/icons/Chat";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import "./Sidebar.css";
+import { IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
         <div className="sidebar__headerRight">
-          <DonutLargeIcon />
-          <ChatIcon />
-          <MoreVertIcon />
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
         <div className="sidebar__headerLeft"></div>
       </div>
