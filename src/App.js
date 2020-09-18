be61b9b58f7c860db2986f91b424e8fd5338d9cb
wildenali <wildenwildenaliali@gmail.com>
2020-09-18 13:33:31 +0700

    import Sidebar.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     // BEM class naming
     <div className="app">
-      <h1>Test</h1>
-
       {/* Sidebar Component */}
+      <Sidebar />
 
       {/* Chat Component */}
     </div>
