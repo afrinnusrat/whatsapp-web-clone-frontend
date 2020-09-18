c9ee1d2d7b77b9cbf6363a871b2f5bec6e543e90
wildenali <wildenwildenaliali@gmail.com>
2020-09-18 14:31:43 +0700

    Add Avatar icon

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,12 +3,15 @@ import "./Sidebar.css";
 import ChatIcon from "@material-ui/icons/Chat";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import { IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
+        <div className="sidebar__headerLeft">
+          <Avatar src="https://avatars3.githubusercontent.com/u/8510612?s=460&u=984021fbf341588e439a344f515c1307e1240e2b&v=4" />
+        </div>
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -20,7 +23,6 @@ function Sidebar() {
             <MoreVertIcon />
           </IconButton>
         </div>
-        <div className="sidebar__headerLeft"></div>
       </div>
     </div>
   );
