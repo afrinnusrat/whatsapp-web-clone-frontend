39e96126e286dcf4d927587b0ee35b8fff37e1e8
wildenali <wildenwildenaliali@gmail.com>
2020-09-18 15:43:01 +0700

    styling for sidebarChat banyak banget

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
@@ -34,6 +35,10 @@ function Sidebar() {
       </div>
 
       <div className="sidebar__chats">
+        <h2>Add new Chat</h2>
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
         <SidebarChat />
       </div>
     </div>
